fd07df09a1f6a85b71750341d575f353487d7886
Briand Alpa <briand.devs@gmail.com>
2026-03-13 00:17:01 +0700

    chore: enable coverage collection in Jest

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  // Transpile TypeScript and JavaScript files using babel-jest
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',
   },
-
-  // Test file patterns
   testMatch: [
     '**/tests/**/*.test.ts',
     '**/src/**/*.test.ts',
   ],
-
-  // Module path aliases (mirrors tsconfig paths)
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-
-  // Test environment
   testEnvironment: 'node',
-
-  // Coverage configuration (commented out - enable when needed)
-  // collectCoverageFrom: [
-  //   'src/**/*.ts',
-  //   '!src/generated/**',
-  //   '!src/**/*.test.ts',
-  // ],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/generated/**',
+    '!src/**/*.test.ts',
+    '!src/main.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+      functions: 90,
+      branches: 85,
+      statements: 90,
+    },
+  },
 };
